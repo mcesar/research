097fba9ba519476b6b038210222c9d4ee2c42e45
Marcos César de Oliveira <mcesarhm@gmail.com>
2015-10-25 00:37:57 -0200

    Fix bug

--- papers/saner2016/cmd/consolidate/consolidate.go
+++ papers/saner2016/cmd/consolidate/consolidate.go
@@ -92,8 +92,8 @@ func main() {
 	}()
 	lookupChangeset := func(dc string) (*structs.Change, string) {
 		comm := dc[strings.Index(dc, " - ")+3:]
-		comm = comm[:strings.Index(dc, " - ")]
-		comm = comm[:strings.Index(dc, " - ")]
+		comm = comm[:strings.LastIndex(comm, " - ")]
+		comm = comm[:strings.LastIndex(comm, " - ")]
 		/*
 			for i := 1; i < len(arr)-2; i++ {
 				if len(comm) > 0 {
