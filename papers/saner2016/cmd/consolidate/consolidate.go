3d406ae1dfd1e2a27fd5c3a41fd963606c2a41d1
Marcos César de Oliveira <mcesarhm@gmail.com>
2015-10-25 00:33:01 -0200

    Add debug info

--- papers/saner2016/cmd/consolidate/consolidate.go
+++ papers/saner2016/cmd/consolidate/consolidate.go
@@ -114,6 +114,7 @@ func main() {
 		key := strings.ToLower(fmt.Sprintf("%v - %v - %v", comm, author, time))
 		c := changesets[key]
 		if c == nil {
+			fmt.Println(dc)
 			log.Fatal("Key not found: ", key)
 		}
 		return c, key
